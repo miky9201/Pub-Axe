b1004f846e1cc345e8407fe1295e60fd24009eff
Mike Baldacci <baldaccimike@gmail.com>
2021-09-16 17:31:04 +0200

    #3 button setup

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ const video = document.getElementById("video-player");
 const videoSection= document.getElementById("video-section");
 const bottomSection= document.getElementById("bottom-section");
 
-video.addEventListener("ended", () => {
+video.addEventListener("click", () => {
       videoSection.className = 'video-disaparition';
+      video.pause();
+      video.currentTime = 0;
       bottomSection.className = 'bottom-up';
-      // videoSection.hidden = true;
+      
 })
