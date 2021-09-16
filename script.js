6396e2c23aa46da4e1049c7faead8e5d4839ab0d
Mike Baldacci <baldaccimike@gmail.com>
2021-09-16 12:01:09 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,3 @@
+const video = document.getElementById("video-player");
+const videoContainer = document.getElementById("video-container");
+video.addEventListener("ended", () => videoContainer.hidden = true)
