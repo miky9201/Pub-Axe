87c6a1e93a7b35e92f1fb6157b11853d668ab293
Mike Baldacci <baldaccimike@gmail.com>
2021-09-16 15:48:11 +0200

    #2 Transition video setup

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 const video = document.getElementById("video-player");
-const videoContainer = document.getElementById("video-container");
+const videoSection= document.getElementById("video-section");
+const bottomSection= document.getElementById("bottom-section");
 
-video.addEventListener("ended", () => videoContainer.hidden = true)
+video.addEventListener("ended", () => {
+      videoSection.className = 'video-disaparition';
+      bottomSection.className = 'bottom-up';
+      // videoSection.hidden = true;
+})
