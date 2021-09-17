a3209b331e12bf970d66fbeb10465916de6918aa
Mike Baldacci <baldaccimike@gmail.com>
2021-09-17 15:01:29 +0200

    #8 Final Commit

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
-const video = document.getElementById("video-player");
-const videoSection = document.getElementById("video-section");
-const bottomSection = document.getElementById("bottom-section");
-const mapSection = document.getElementById("map-section");
-const goButton = document.getElementById("go-button");
-const profiteButton = document.getElementById("profite-button");
-const arrow = document.getElementById("arrow");
-const address = document.getElementById("address");
+const  DOM_ELEMENTS = {
+      video: document.getElementById("video-player"),
+      videoSection: document.getElementById("video-section"),
+      bottomSection: document.getElementById("bottom-section"),
+      mapSection: document.getElementById("map-section"),
+      goButton: document.getElementById("go-button"),
+      profiteButton: document.getElementById("profite-button"),
+      arrow: document.getElementById("arrow"),
+      address: document.getElementById("address")
+}
 
 // Initial Application State
-mapSection.style.display = "none";
-goButton.style.display = "none";
+DOM_ELEMENTS.mapSection.style.display = "none";
+DOM_ELEMENTS.goButton.style.display = "none";
 
+// Function for map initialization
 function initMap()  {
 
       // The fake location (Nimes)
@@ -32,24 +35,27 @@ function initMap()  {
       const getAddress = async (location) => {
             await fetch(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${location.lat},${location.lng}&key=AIzaSyD1NSn_5WguHll7dJ3wA7qO-BVs4RDyRsU`)
             .then( res => res.json())
-            .then( data => address.innerHTML = data.results[0].formatted_address )
+            .then( data => DOM_ELEMENTS.address.innerHTML = "Leclerc, " + data.results[0].formatted_address )
       }
       getAddress(location)
 }
 
 // All DOM interactions either on video's end or button's click.
 const pubInteractions = () => {
-      mapSection.style.removeProperty("display");
-      goButton.style.removeProperty("display");
-      arrow.style.display = "none";
-      goButton.classList.add("button-up");
-      bottomSection.className = "bottom-up";
-      videoSection.className = "video-disaparition";
-      video.pause();
-      video.currentTime = 0;
+
+      DOM_ELEMENTS.mapSection.style.removeProperty("display");
+      DOM_ELEMENTS.goButton.style.removeProperty("display");
+      DOM_ELEMENTS.arrow.style.display = "none";
+
+      DOM_ELEMENTS.videoSection.className = "video-disaparition";
+      DOM_ELEMENTS.goButton.classList.add("button-up");
+      DOM_ELEMENTS.bottomSection.className = "bottom-up";
+      
+      DOM_ELEMENTS.video.pause();
+      DOM_ELEMENTS.video.currentTime = 0;
 }
 
-// EventListener when video is ended and the button clicked.
-video.addEventListener("ended", pubInteractions)
-profiteButton.addEventListener("click", pubInteractions)
+// EventListener for video's end and button's click.
+DOM_ELEMENTS.video.addEventListener("ended", pubInteractions)
+DOM_ELEMENTS.profiteButton.addEventListener("click", pubInteractions)
 
