bbebc35d7898db2cad65b35b2491eff4e35b750b
Mike Baldacci <baldaccimike@gmail.com>
2021-09-17 10:37:54 +0200

    #5 Button animation setup

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 const video = document.getElementById("video-player");
-const videoSection= document.getElementById("video-section");
-const bottomSection= document.getElementById("bottom-section");
+const videoSection = document.getElementById("video-section");
+const bottomSection = document.getElementById("bottom-section");
+const mapSection = document.getElementById("map-section");
+const goButton = document.getElementById("go-button");
+const arrow = document.getElementById("arrow");
+
+mapSection.style.display = "none";
+goButton.style.display = "none";
 
 
 video.addEventListener("ended", () => {
-      videoSection.className = 'video-disaparition';
+      mapSection.style.removeProperty("display");
+      goButton.style.removeProperty("display");
+      arrow.style.display = "none";
+      goButton.classList.add("button-up");
+      bottomSection.className = "bottom-up";
+      videoSection.className = "video-disaparition";
       video.pause();
       video.currentTime = 0;
-      bottomSection.className = 'bottom-up';
-      
 })
